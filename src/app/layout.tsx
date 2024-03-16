0eb43d72f47f83a70548b9ad13940da7a9c498e0
wasupon <wasupon.mail@gmail.com>
2024-03-17 00:49:31 +0900

    edit: h1 text

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
         className={`${inter.className} bg-gradient-to-r from-slate-900 to-indigo-900`}
       >
         <nav>
-          <h1 className="text-white">Recipes for Vegan</h1>
+          <h1 className="text-white">Recipes and Foods</h1>
         </nav>
         {children}
       </body>
